6b1e11ab0e4112defa2702713a65fe4eeb560728
macbookpro <Devshoh>
2023-07-19 21:18:51 +0500

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:tutorchat/Pages/HomePage/homeScreen.dart';
 
-import 'package:tutorchat/splash.dart';
+import 'package:tutorchat/Pages/languagePage/languageScreen.dart';
+import 'package:tutorchat/controllerPage/controllerPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyCustomSplashScreen(),
+      home: ControllerPage(),
     );
   }
 }
