8425edfbe6f3558b72a700bd431fb4d380478a9e
macbookpro <Devshoh>
2023-07-29 10:26:06 +0500

    textStyle.dart

---
+++ lib/widgets/textStyle.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/cupertino.dart';
+
+TextStyle textStyle(FontWeight fontWeight,double fontSize, Color color, String fontFamily){
+  return TextStyle(fontSize: fontSize, color: color,fontWeight: fontWeight,fontFamily: fontFamily);
+}
