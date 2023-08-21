62bda3163e9f9c6259468f26b55a1ebef8eb9be2
macbookpro <Devshoh>
2023-08-21 23:14:25 +0500

    collectionItem.dart

---
+++ lib/Pages/collectionPage/collectionItem.dart
@@ -0,0 +1,38 @@
+class CollectionItem{
+  String image;
+  String subjectname;
+  int videoscounter;
+  int likecounter;
+  int viewscounter;
+  bool delete = false;
+  CollectionItem(this.delete,{required this.image,required this.subjectname,required this.videoscounter, required this.likecounter, required this.viewscounter});
+}
+List<CollectionItem> collectionItem=[
+  CollectionItem(false,image: 'assets/png/chemistry.jpeg', subjectname: 'Matematika', videoscounter: 123, likecounter: 45, viewscounter: 123),
+  CollectionItem(false,image: 'assets/png/chemistry.jpeg', subjectname: 'Yosh matematik', videoscounter: 123, likecounter: 45, viewscounter: 123),
+  CollectionItem(false,image: 'assets/png/chemistry.jpeg', subjectname: 'Kimyogarlar', videoscounter: 123, likecounter: 45, viewscounter: 123),
+  CollectionItem(false,image: 'assets/png/chemistry.jpeg', subjectname: 'Fiziklar2', videoscounter: 123, likecounter: 45, viewscounter: 123),
+  CollectionItem(false,image: 'assets/png/chemistry.jpeg', subjectname: 'Sa`nat', videoscounter: 123, likecounter: 45, viewscounter: 123),
+  CollectionItem(false,image: 'assets/png/chemistry.jpeg', subjectname: 'Fiziklar', videoscounter: 123, likecounter: 45, viewscounter: 123),
+  CollectionItem(false,image: 'assets/png/chemistry.jpeg', subjectname: 'Kinochilar', videoscounter: 123, likecounter: 45, viewscounter: 123),
+  CollectionItem(false,image: 'assets/png/chemistry.jpeg', subjectname: 'Musiqa sanati', videoscounter: 123, likecounter: 45, viewscounter: 123),
+  CollectionItem(false,image: 'assets/png/chemistry.jpeg', subjectname: 'Musiqa', videoscounter: 123, likecounter: 4589, viewscounter: 123),
+  CollectionItem(false,image: 'assets/png/chemistry.jpeg', subjectname: 'Kimyo', videoscounter: 1233, likecounter: 48885, viewscounter: 123),
+  CollectionItem(false,image: 'assets/png/chemistry.jpeg', subjectname: 'Jismoniy Tarbiya', videoscounter: 3, likecounter: 45, viewscounter: 123),
+  CollectionItem(false,image: 'assets/png/chemistry.jpeg', subjectname: 'Fizika', videoscounter: 123, likecounter: 45, viewscounter: 123),
+  CollectionItem(false,image: 'assets/png/chemistry.jpeg', subjectname: 'Ona tili', videoscounter: 123, likecounter: 450998, viewscounter: 123),
+  CollectionItem(false,image: 'assets/png/chemistry.jpeg', subjectname: 'Falsafa', videoscounter: 132, likecounter: 4523, viewscounter: 123),
+  CollectionItem(false,image: 'assets/png/chemistry.jpeg', subjectname: 'Manaviyat', videoscounter: 123, likecounter: 45, viewscounter: 123),
+  CollectionItem(false,image: 'assets/png/chemistry.jpeg', subjectname: 'Kimyo2', videoscounter: 123, likecounter: 45, viewscounter: 123),
+  CollectionItem(false,image: 'assets/png/chemistry.jpeg', subjectname: 'Dasturlash2', videoscounter: 18, likecounter: 45, viewscounter: 123),
+  CollectionItem(false,image: 'assets/png/chemistry.jpeg', subjectname: 'Dasturlash', videoscounter: 123, likecounter: 45, viewscounter: 123),
+  CollectionItem(false,image: 'assets/png/chemistry.jpeg', subjectname: 'Informatika', videoscounter: 12, likecounter: 45, viewscounter: 123),
+  CollectionItem(false,image: 'assets/png/chemistry.jpeg', subjectname: 'Kinematika', videoscounter: 123, likecounter: 45, viewscounter: 123),
+  CollectionItem(false,image: 'assets/png/chemistry.jpeg', subjectname: 'Adabiyot', videoscounter: 123, likecounter: 45, viewscounter: 123),
+  CollectionItem(false,image: 'assets/png/chemistry.jpeg', subjectname: 'Oliy matematika', videoscounter: 123, likecounter: 45, viewscounter: 123),
+  CollectionItem(false,image: 'assets/png/chemistry.jpeg', subjectname: 'Maktabgacha tayyorlash', videoscounter: 778, likecounter: 45, viewscounter: 123),
+  CollectionItem(false,image: 'assets/png/chemistry.jpeg', subjectname: 'Litseygacha tayyorlash', videoscounter: 80, likecounter: 45, viewscounter: 123),
+  CollectionItem(false,image: 'assets/png/chemistry.jpeg', subjectname: 'Universitetga tayyorlash', videoscounter: 9, likecounter: 45, viewscounter: 123),
+  CollectionItem(false,image: 'assets/png/chemistry.jpeg', subjectname: 'Amaliyotga tayyorlash', videoscounter: 89, likecounter: 45, viewscounter: 123),
+  CollectionItem(false,image: 'assets/png/chemistry.jpeg', subjectname: 'Maktabgacha2 tayyorlash', videoscounter: 8090, likecounter: 45, viewscounter: 123),
+];
